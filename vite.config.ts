2233260413981d0202e2d5ece4b9005d11014ec7
Radjab Gabibov <kanalori@mail.ru>
2024-08-18 23:00:41 +0300

    feat(vite.config.ts): added prod support

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,13 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import svgLoader from 'vite-svg-loader';
 
+const isProd: boolean = process?.env?.NODE_ENV === 'production';
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(), svgLoader()],
+
+    base: isProd ? '/zhilfond-test/' : '/',
 
     css: {
         preprocessorOptions: {
